5febdd0c75edf8ebc112aefab6b6204690177ee5
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2022-11-20 22:06:11 +0100

    add return in Result component

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,8 +1,9 @@
-const Result = ({ result }) => (
-    <p className="form__result">Twój wynik:
-        <strong> {result.targetAmount}&nbsp;{result.currency}</strong>
+const Result = ({ result }) => {
+    return(
+    <p className="form__result">Twój wynik: {<strong> {result.targetAmount}&nbsp;{result.currency}</strong>}
+        
     </p>
 
-);
+)};
 
 export default Result;

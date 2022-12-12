938920b7b0fc4d17ca40e4d715db2a7482f61614
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2022-12-12 19:43:12 +0100

    add "N/A" to Result Component

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,6 +1,9 @@
 const Result = ({ result }) => {
     return(
-    <p className="form__result">Twój wynik: {<strong> {result.targetAmount}&nbsp;{result.currency}</strong>}
+    <p className="form__result">Twój wynik: {<strong> 
+        {result.targetAmount}&nbsp;
+        {result.currency ? result.currency : "N/A"}
+        </strong>}
         
     </p>
 

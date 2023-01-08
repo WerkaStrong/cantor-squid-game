edec8a30d5a024d9dfc7b57c4e1183fb6588dd80
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-01-08 22:31:31 +0100

    create clock component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Container from "./Container";
 import Form from "./Form";
 import Header from "./Header";
+import Clock from "./Clock";
 
 function App() {
 
   return (
     <Container>
       <section className="calculatorField">
+      <Clock />
         <Header
           title="통화를 변환" />
         <Form/>

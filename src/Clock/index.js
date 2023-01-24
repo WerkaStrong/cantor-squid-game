f7754538f18d18982c60d072c5b8903589149c6e
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-01-24 15:17:20 +0100

    changed function to arrow function in Clock comp.

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,33 +1,31 @@
 import { useState, useEffect } from "react";
 import "./style.css";
 
-function Clock() {
+const Clock = () => {
     const [date, setDate] = useState(new Date());
 
-    function refreshClock() {
-        setDate(new Date());
-    }
-
     useEffect(() => {
-        const timerId = setInterval(refreshClock, 1000);
-        return function cleanup() {
-            clearInterval(timerId);
-        };
+        const timerId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => clearInterval(timerId);
     }, []);
 
-    return <span className="dateAndClock">
-        Dzisiaj jest
-        {' '}
-        {date.toLocaleDateString("pl-PL", {
-            weekday: 'long',
-            day: 'numeric',
-            month: 'long',
-            year: 'numeric',
-        })}
-        {' '}
-        {date.toLocaleTimeString()}
-
-    </span>;
+    return (
+        <span className="dateAndClock">
+            Dzisiaj jest
+            {' '}
+            {date.toLocaleDateString("pl-PL", {
+                weekday: 'long',
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+            })}
+            {' '}
+            {date.toLocaleTimeString()}
+        </span>
+    );
 }
 
 export default Clock;

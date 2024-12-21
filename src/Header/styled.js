49df9762a8ab6bdf81905b51b33938e57b23fef6
WerkaStrong <weronika.chmieleska00@gmail.com>
2024-12-21 18:26:06 +0100

    changed font size of header

--- src/Header/styled.js
+++ src/Header/styled.js
@@ -1,7 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const CalculatorHeader = styled.h1`
-    font-size: 30px;
+    font-size: 60px;
     letter-spacing: 2px;
-    font-family: "Noto Sans KR";
-`
+    font-family: 'Noto Sans KR';
+`;
